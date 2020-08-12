42e5c9a865f5f570c799718696306ab4f27ccb06
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-08-12 13:02:26 -0500

    diseños finales

--- disenos/lib/src/pages/botones_page.dart
+++ disenos/lib/src/pages/botones_page.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -7,13 +8,21 @@ class BotonesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Stack(        
         children: [
           _fondoapp(),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                _titutlos(),
+                _botonesRedondeados(),
+              ],
+            ),
+          ),
         ],
 
       ),
-      
+      bottomNavigationBar:_buttonnavigationBar(context)      
     );
   }
 
@@ -61,6 +70,112 @@ class BotonesPage extends StatelessWidget {
           child:  cajaRosa,
         )
       ],
+    );
+  }
+
+  Widget _titutlos(){
+    return SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Clasify hola", style: TextStyle(color: Colors.white, fontSize: 30.0,fontWeight:FontWeight.bold ),),
+              SizedBox(height: 10.0,),
+              Text("classify this transaction into a popular ",style: TextStyle(color: Colors.white, fontSize: 18.0 )) 
+            ],
+          ),
+
+      ),
+    );
+  }
+
+Widget _buttonnavigationBar(BuildContext context){
+  return Theme(
+    data: Theme.of(context).copyWith(
+      canvasColor: Color.fromRGBO(45, 57, 840, 1.0),
+      primaryColor: Colors.pinkAccent,
+      textTheme: Theme.of(context).textTheme.copyWith(
+        caption: TextStyle(color: Color.fromRGBO(116, 117, 152, 1.0))
+      ),
+    ),
+    child: BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.sentiment_neutral),
+          title: Container(),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.sentiment_satisfied),
+          title: Container(),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.sentiment_very_dissatisfied),
+          title: Container(),
+        ),
+      ],
+    ),
+  );
+
+}
+
+  Widget _botonesRedondeados(){
+    return Table(
+      children: [
+        TableRow(
+          children: [
+            _crearBotnRedondeado(Colors.blue,Icons.settings_brightness,"general"),
+            _crearBotnRedondeado(Colors.purpleAccent,Icons.settings_input_svideo,"video"),
+          ]
+        ),
+        TableRow(
+          children: [
+            _crearBotnRedondeado(Colors.redAccent,Icons.opacity,"hola"),
+            _crearBotnRedondeado(Colors.blue,Icons.settings_brightness,"general"),
+          ]
+        ),
+        TableRow(
+          children: [
+            _crearBotnRedondeado(Colors.deepOrange,Icons.settings_brightness,"general"),
+            _crearBotnRedondeado(Colors.white10,Icons.settings_brightness,"general"),
+          ]
+        ),
+        TableRow(
+          children: [
+            _crearBotnRedondeado(Colors.tealAccent,Icons.settings_brightness,"general"),
+            _crearBotnRedondeado(Colors.orange,Icons.settings_brightness,"general"),
+          ]
+        ),
+      ],
+    );  
+  }
+
+  Widget _crearBotnRedondeado(Color color, IconData icono,String string ){
+
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur( ),//consume recursos 
+        child: Container(
+          height: 180.0,
+          margin: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(color: Color.fromRGBO(62, 66, 107, 0.7),
+          borderRadius: BorderRadius.circular(20.0)
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(height: 5.0,),
+              CircleAvatar(
+                backgroundColor: color,
+                radius: 35.0,
+                child: Icon(icono, color: Colors.white, size: 30.0,),            
+              ),
+              Text(string, style: TextStyle(color: color), ),
+              SizedBox(height: 5.0,),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
